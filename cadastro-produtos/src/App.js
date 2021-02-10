938f00fadeef8ebdf2f358dec7df5d014a428cd9
Caio Cesar <caio.sfg@gmail.com>
2021-02-10 00:43:37 -0300

    Bootswatch importado

---
+++ cadastro-produtos/src/App.js
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <div class="alert alert-dismissible alert-primary">
+      <button type="button" class="close" data-dismiss="alert">&times;</button>
+      <strong>Oh snap!</strong> <a href="#" class="alert-link">Change a few things up</a> and try submitting again.
+    </div>
+  );
+}
+
+export default App;
